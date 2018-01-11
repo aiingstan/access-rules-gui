5a35d6ec8ae6c133251d6bcff16f3ab259685473
Yufeng Guo <guo_yufeng@outlook.com>
2018-01-11 16:50:25 +0800

    groups page & vue resource

---
+++ src/components/Groups.vue
@@ -0,0 +1,33 @@
+<template lang='jade'>
+  .groups
+    router-link(to='/') Home
+    div(v-for='group in groups')
+      p {{ group[0] }}
+</template>
+
+<script>
+export default {
+  name: 'Groups',
+  data () {
+    return {
+      groups: []
+    }
+  },
+  mounted () {
+    console.log('mounted')
+    this.$http.get('http://localhost:8080/static/data/parsed-data.json')
+      .then(response => {
+        return response.json()
+      }, response => {
+        console.log(response)
+      })
+      .then(data => {
+        this.groups = data.groups
+      })
+      .catch(err => {
+        debugger
+        console.log(err)
+      })
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueResource from 'vue-resource'
 import Navigation from '@/components/Navigation'
 import Help from '@/components/Help'
+import Groups from '@/components/Groups'
 
 Vue.use(Router)
+Vue.use(VueResource)
 
 export default new Router({
   routes: [
@@ -11,6 +14,11 @@ export default new Router({
       path: '/',
       name: 'Navigation',
       component: Navigation
+    },
+    {
+      path: '/groups',
+      name: 'Groups',
+      component: Groups
     },
     {
       path: '/help',
